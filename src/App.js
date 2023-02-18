0ed60e24dede34508886ad2bba85278ce1003cae
Rameez Ahmed Ansari <90474804+rameezahmedansari@users.noreply.github.com>
2023-02-18 20:02:59 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import html2pdf from "html2pdf.js";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  handlePdfConversion = () => {
+    // Get the HTML element that you want to convert to PDF
+    const element = document.getElementById("my-element");
+
+    // Set the options for the PDF conversion
+    const options = {
+      filename: "web.pdf",
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+    };
+
+    // Convert the HTML element to PDF
+    html2pdf().from(element).set(options).save();
+  };
+
+  render() {
+    return (
+      <div className="heading">
+        <h1>Web Page convertor to PDF</h1>
+        <p>web page below content will converted in pdf</p>
+        <button className="btn" onClick={this.handlePdfConversion}>
+          Convert to PDF <i className="fa fa-file-pdf-o"></i>
+        </button>
+        <div id="my-element">
+          <p>
+            What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the
+            leap into electronic typesetting, remaining essentially unchanged.
+            It was popularised in the 1960s with the release of Letraset sheets
+            containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of Lorem
+            Ipsum.
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
